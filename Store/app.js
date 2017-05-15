2725aa0fd1f22baa4f15e11772f5b72664225cd7
= <=>
2017-05-15 14:40:42 +0200

    =Shopping store

---
+++ Store/app.js
@@ -0,0 +1,41 @@
+(function(){
+  'use strict';
+
+  var shoppingListOne = ['vino', 'verdura', 'latte', 'pesce'];
+
+  var shoppingListTwo = [
+    {
+      name: 'vino',
+      quantity: 2
+    },
+    {
+      name: 'verdura',
+      quantity: 3
+    },
+    {
+      name: 'latte',
+      quantity: 4
+    }
+  ];
+
+  angular.module('OnlineStoreApp', [])
+         .controller('OnlineStoreController', OnlineStoreController);
+
+  OnlineStoreController.$inject = ['$scope'];
+  function OnlineStoreController($scope){
+    $scope.shoppingListOne = shoppingListOne;
+    $scope.shoppingListTwo = shoppingListTwo;
+
+    $scope.newItemName = "";
+    $scope.newItemQuantity = "";
+
+    $scope.addItem = function(){
+      var item = {
+        name: $scope.newItemName,
+        quantity: $scope.newItemQuantity
+      };
+      $scope.shoppingListTwo.push(item);
+    }
+  }
+
+})();
